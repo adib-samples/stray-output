cac75029a8dae2090e4363f542e9e863a3c7171c
Adib Saikali <asaikali@gmail.com>
2020-03-04 09:59:50 -0500

    initial commit

---
+++ src/main/java/com/example/strayoutput/StrayOutputApplication.java
@@ -0,0 +1,13 @@
+package com.example.strayoutput;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StrayOutputApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(StrayOutputApplication.class, args);
+  }
+
+}
---
+++ src/test/java/com/example/strayoutput/StrayOutputApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.strayoutput;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StrayOutputApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
